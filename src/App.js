c020562d56c0a440bde2fbd1970b13a793ed1354
Benny Wang <bennywang75@hotmail.com>
2019-02-28 13:43:02 -0800

    Finished example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HomeComponent from './HomeComponent.js'
+import SetComponent from './SetComponent.js'
+import ItemComponent from './ItemComponent.js'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Router>
+        <div className="App">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div>
+            <Link className="App-link" to="/">Home</Link>
+            <Link className="App-link" to="/set">Sets</Link>
+            <Link className="App-link" to="/item">Items</Link>
+          </div>
+          <div>
+            <Route exact path='/' component={HomeComponent} />
+            <Route exact path='/set' component={SetComponent} />
+            <Route exact path='/item' component={ItemComponent} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/ItemComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ItemComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default ItemComponent;
